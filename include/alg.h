5767d65b50571c15f9870aa61c8c99a4d176ec2e
Daniil <Fedotov.D.V2006@mail.ru>
2026-02-25 13:24:28 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -4,9 +4,11 @@
 #include <cstdint>
 
 bool checkPrime(uint64_t value) {
-    if (value < 2) return false;
+    if (value < 2)
+        return false;
     for (uint64_t i = 2; i * i <= value; i++) {
-        if (value % i == 0) return false;
+        if (value % i == 0)
+            return false;
     }
     return true;
 }
@@ -15,26 +17,30 @@ uint64_t nPrime(uint64_t n) {
     for (uint64_t i = 2;; i++) {
         if (checkPrime(i)) {
             a++;
-            if (a == n) return i;
+            if (a == n)
+                return i;
         }
     }
 }
 uint64_t nextPrime(uint64_t value){
   for (uint64_t i = value + 1;; i++) {
-    if (checkPrime(i)) return i;
+    if (checkPrime(i))
+        return i;
   }
 }
 uint64_t sumPrime(uint64_t hbound){
   uint64_t b = 0;
   for (uint64_t i = 2; i < hbound; i++) {
-    if (checkPrime(i)) b += i;
+    if (checkPrime(i))
+        b += i;
   }
     return b;
 }
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound){
   uint64_t c = 0;
   for (uint64_t i = lbound; i + 2 < hbound; i++) {
-    if (checkPrime(i) && checkPrime(i + 2)) c++;
+    if (checkPrime(i) && checkPrime(i + 2))
+        c++;
   }
   return c;
 }
