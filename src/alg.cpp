f9178555e763b78bff6aef5f566f2a40c6310f7d
Daniil <Fedotov.D.V2006@mail.ru>
2026-02-25 14:10:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,9 +2,7 @@
 #include <cstdint>
 #include "alg.h"
 
-#include "alg.h"
-
-bool checkPrime (uint64_t vslue) {
+bool checkPrime(uint64_t value) {
     if (value < 2)
       return false;
     for (uint64_t i = 2; i * i <= value; i++) {
@@ -14,7 +12,7 @@ bool checkPrime (uint64_t vslue) {
     return true;
 }
 
-uint64_t nPrime (uint64_t n) {
+uint64_t nPrime(uint64_t n) {
     uint64_t a = 0;
     for (uint64_t i = 2;; i++) {
         if (checkPrime(i)) {
@@ -25,14 +23,14 @@ uint64_t nPrime (uint64_t n) {
     }
 }
 
-uint64_t nextPrime (uint64_t value) {
+uint64_t nextPrime(uint64_t value) {
     for (uint64_t i = value + 1;; i++) {
         if (checkPrime(i))
           return i;
     }
 }
 
-uint64_t sumPrime (uint64_t hbound) {
+uint64_t sumPrime(uint64_t hbound) {
     uint64_t b = 0;
     for (uint64_t i = 2; i < hbound; i++) {
         if (checkPrime(i)) b += i;
@@ -42,7 +40,7 @@ uint64_t sumPrime (uint64_t hbound) {
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
     uint64_t c = 0;
-    for (uint64_t i = l; i + 2 < h; i++) {
+    for (uint64_t i = lbound; i + 2 < hbound; i++) {
         if (checkPrime(i) && checkPrime(i + 2)) c++;
     }
     return c;
