2b07541726f8fd0ebfb98dda1453bfce84bcc5f3
Daniil <Fedotov.D.V2006@mail.ru>
2026-02-25 14:02:33 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -1,48 +1,10 @@
-// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_ALG_H_
 #define INCLUDE_ALG_H_
 #include <cstdint>
 
-bool checkPrime(uint64_t value) {
-    if (value < 2)
-        return false;
-    for (uint64_t i = 2; i * i <= value; i++) {
-        if (value % i == 0)
-            return false;
-    }
-    return true;
-}
-uint64_t nPrime(uint64_t n) {
-    uint64_t a = 0;
-    for (uint64_t i = 2;; i++) {
-        if (checkPrime(i)) {
-            a++;
-            if (a == n)
-                return i;
-        }
-    }
-}
-uint64_t nextPrime(uint64_t value){
-  for (uint64_t i = value + 1;; i++) {
-    if (checkPrime(i))
-        return i;
-  }
-}
-uint64_t sumPrime(uint64_t hbound){
-  uint64_t b = 0;
-  for (uint64_t i = 2; i < hbound; i++) {
-    if (checkPrime(i))
-        b += i;
-  }
-    return b;
-}
-uint64_t twinPrimes(uint64_t lbound, uint64_t hbound){
-  uint64_t c = 0;
-  for (uint64_t i = lbound; i + 2 < hbound; i++) {
-    if (checkPrime(i) && checkPrime(i + 2))
-        c++;
-  }
-  return c;
-}
-
+bool checkPrime (uint64_t value);
+uint64_t nPrime (uint64_t n);
+uint64_t nextPrime (uint64_t value);
+uint64_t sumPrime (uint64_t hbound);
+uint64_t twinPrimes (uint64_t lbound, uint64_t hbound);
 #endif  // INCLUDE_ALG_H_
